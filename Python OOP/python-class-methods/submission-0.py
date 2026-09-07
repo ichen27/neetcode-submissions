be8b7e88fd7f428ae42a7b8785a5333d1f2ce4c0
Ivan Chen <107697782+ichen27@users.noreply.github.com>
2026-09-07 15:09:47 -0400

    Add: python-class-methods - submission-0

---
+++ Python OOP/python-class-methods/submission-0.py
@@ -0,0 +1,23 @@
+class Library:
+    books_available = 100    # Total books in library
+
+    # TODO: Implement class methods to manage book lending
+    # TODO: Implement return_books method to increase the number of books available
+
+    @classmethod
+    def lend_books(cls, books) -> None:
+        cls.books_available -= books
+        return
+
+    @classmethod
+    def return_books(cls, books) -> None:
+        cls.books_available += books
+        return
+
+
+# Don't change the code below
+print(f"Initial status: {Library.books_available} books available")
+Library.lend_books(30)
+print(f"After lending: {Library.books_available} books available")
+Library.return_books(10)
+print(f"After return: {Library.books_available} books available")
